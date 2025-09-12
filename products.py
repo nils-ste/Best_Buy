dd31bacdf4c487563cc9564ac1f05475bd32781a
Nils Steinmetz <nils_steinmetz@web.de>
2025-09-12 15:59:25 +0200

    implemented product class

---
+++ products.py
@@ -0,0 +1,43 @@
+class Product:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.active = True
+
+        if type(name) != str or name == "":
+            raise TypeError("Name must be a string and cannot be empty")
+        if price < 0:
+            raise ValueError("Price cannot be negative")
+        if quantity < 0:
+            raise ValueError("Quantity cannot be negative")
+
+    def get_quantity(self):
+        return self.quantity
+
+    def set_quantity(self, quantity):
+        self.quantity = quantity
+        if self.quantity == 0:
+            self.active = False
+
+    def is_active(self):
+        return self.active
+
+    def activate(self):
+        self.active = True
+
+    def deactivate(self):
+        self.active = False
+
+    def show(self):
+        print(f"{self.name}, Price: {self.price}, Quantity: {self.quantity}")
+
+    def buy(self, quantity):
+        if quantity <= 0:
+            raise ValueError("Quantity cannot be negative or None")
+        self.quantity -= quantity
+        total_price = quantity * self.price
+        return total_price
+
+
+
